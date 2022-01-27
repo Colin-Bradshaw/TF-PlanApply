9c500dba36cf8fc21998a9cdf10800810c81a4b3
Colin-Bradshaw <90667786+Colin-Bradshaw@users.noreply.github.com>
2022-01-26 23:16:16 -0500

    Update config.tf

--- config.tf
+++ config.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "tf-utopia-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+  }
 }
 
 provider "aws" {
