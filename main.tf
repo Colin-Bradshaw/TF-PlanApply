31a842bd951cd0487a234339ffcd1e712a5a6478
Colin-Bradshaw <90667786+Colin-Bradshaw@users.noreply.github.com>
2022-01-26 23:28:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,23 +47,6 @@ resource "aws_s3_bucket" "utopia-bucket" {
     Name = "utopia_bucket"
   }
 }
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tf-utopia-state"
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
 	
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
